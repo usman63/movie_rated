a3c4b14892d3d265f995c00712084550f04d2744
usman <usman.saleem@square63.com>
2016-06-27 17:03:55 +0500

    User: updated the view files and  added  the confirmable functionality for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -38,4 +39,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
+
 end
