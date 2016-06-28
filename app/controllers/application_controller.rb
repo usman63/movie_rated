aca51c018945e085e56ca44216070c1d3885e05d
usman <usman.saleem@square63.com>
2016-06-28 10:23:59 +0500

    pages : added  pages controller to redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
+  def after_sign_in_path_for(resource)
+    home_path
+  end
 
   protected
 
