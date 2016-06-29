c8d6f574583be755022627d8bf2106547593a5b0
usman <usman.saleem@square63.com>
2016-06-29 09:40:52 +0500

    Movies: Added validations to movies model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+  validates :name,          presence: true, length: { maximum: 60 }
+  validates :released_date, presence: true
+  validates :is_featured,   presence: true,                         default: false
+  validates :duration,                      length: { maximum: 20 }
 end
