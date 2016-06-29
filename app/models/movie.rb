94bf547df19e84e0d0e762bafd3389e376418058
usman <usman.saleem@square63.com>
2016-06-29 13:09:55 +0500

    Remove field : removing field is_featured and also its validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,6 +6,5 @@ class Movie < ActiveRecord::Base
 
   validates :name,          presence: true, length: { maximum: 60 }
   validates :released_date, presence: true
-  validates :is_featured,   presence: true,                         default: false
   validates :duration,                      length: { maximum: 20 }
 end
