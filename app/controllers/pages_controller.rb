6c6070f2a40e92c7696f5df21ae8ddda14dc6015
usman <usman.saleem@square63.com>
2016-07-02 17:34:49 +0500

    Home : added features of latest and featured

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
   def home
+    @featured_movies = Movie.featured.take(Movie::MOVIE_LMIIT)
+    @latest_movies = Movie.latest.take(Movie::MOVIE_LMIIT)
   end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
-  paginates_per 10
+  paginates_per 2
+
+  MOVIE_LMIIT = 3
   GENRE = ["Horror", "Thriller", "Action", "Comedy"]
 
   has_many :images, as: :imageable
@@ -8,9 +10,18 @@ class Movie < ActiveRecord::Base
 
   accepts_nested_attributes_for :images, allow_destroy: true
 
+  scope :featured, -> { where(featured: true) }
+  scope :latest, -> { order(released_date: :desc) }
+
   validates :name,          presence: true, length: { maximum: 60 }
   validates :released_date, presence: true
   validates :duration,                      length: { maximum: 20 }
   validates :genre,                         length: { maximum: 30 }
+
+  def self.get_movies (params)
+    return self.featured if params[:featured].present?
+    return self.latest if params[:latest].present?
+    return self.all
+  end
+
 end
-  
