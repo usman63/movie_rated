d02cf6cd834dfdf99e1a6fdd90a256e69f344fe6
usman <usman.saleem@square63.com>
2016-07-20 15:55:21 +0500

    Redirection : Preserve current page on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :store_current_location, unless: :devise_controller?
 
   def after_sign_out_path_for(resource)
     new_user_session_path
-  end
-
-  def after_sign_in_path_for(resource)
-    home_path
   end
 
   protected
@@ -18,6 +15,10 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :first_name, :last_name, image_attributes: :image])
     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :current_password, :password, :password_confirmation, :first_name, :last_name, image_attributes: [:id, :image]])
+  end
+
+  def store_current_location
+    store_location_for(:user, request.url)
   end
 
 end
